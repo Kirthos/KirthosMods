4b32c04fa0c6cf969f37dc84737636603cefaa89
Kirthos <gm.kirthos@gmail.com>
2018-10-19 08:57:34 +0200

    Updating RecipeRemover to take care of the skill benefit tooltips

--- Usefull class/RecipeRemover.cs
+++ Usefull class/RecipeRemover.cs
@@ -1,4 +1,5 @@
 using Eco.Gameplay.Components;
+using Eco.Gameplay.DynamicValues;
 using Eco.Gameplay.Items;
 using Eco.Gameplay.Skills;
 using Eco.Gameplay.Systems.TextLinks;
@@ -14,6 +15,7 @@ using System.Reflection;
  * Copyright (c) 2018 [Kirthos]
  * 
  * Created by Kirthos 09/29/2018
+ * Updated by Kirthos 10/19/2018
  */
 
 namespace Kirthos.Mods.Utils
@@ -67,13 +69,19 @@ namespace Kirthos.Mods.Utils
                 // After removing the recipe we need to update the skill unlock
 
                 // Get all the skillunlock tooltips
-                Dictionary<Type, Dictionary<int, List<LocString>>> skillUnlocksTooltips = (Dictionary<Type, Dictionary<int, List<LocString>>>)typeof(Skill).GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(x => x.Name.Contains("skillUnlocksTooltips")).GetValue(Activator.CreateInstance(typeof(ResearchEfficiencySkill)));
+                Dictionary<Type, Dictionary<int, List<LocString>>> skillUnlocksTooltips = (Dictionary<Type, Dictionary<int, List<LocString>>>)typeof(Skill).GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(x => x.Name.Contains("skillUnlocksTooltips")).GetValue(null);
+
+                // Get all the skillbenefit tooltips
+                Dictionary<Type, Dictionary<LocString, List<SkillModifiedValue>>> skillBenefitsTooltips = (Dictionary<Type, Dictionary<LocString, List<SkillModifiedValue>>>)typeof(SkillModifiedValueManager).GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(x => x.Name.Contains("skillBenefits")).GetValue(null);
 
                 // Get the skill that unlock the recipe
                 Type skillType = RequiresSkillAttribute.Cache.Get(targetRecipeType).FirstOrDefault()?.SkillItem.Type;
 
                 // Get the level that unlock the recipe
                 int? recipeUnlockLevel = RequiresSkillAttribute.Cache.Get(targetRecipeType).FirstOrDefault()?.Level;
+
+                // Get the loc string for the recipe
+                LocString locStr = new LocString(Text.Indent(targetRecipe.UILink()));
 
                 // If there is a require skill
                 if (skillType != null)
@@ -84,17 +92,70 @@ namespace Kirthos.Mods.Utils
                     // Search the correct unlock
                     for (int i = 0; i < unlocks.Count; i++)
                     {
-                        if (unlocks[i] == new LocString(Text.Indent(targetRecipe.UILink())))
+                        if (unlocks[i] == locStr)
                         {
-                            // remove the loc string from the lsit
+                            // remove the loc string from the list
                             unlocks.RemoveAt(i);
                             break;
                         }
                     }
                     // Set the new unlock list
                     skillUnlocksTooltips[skillType][recipeUnlockLevel.Value] = unlocks;
+
+                    // For testing
+                    //List<Type> KeyA = new List<Type>();
+                    //List<LocString> KeyB = new List<LocString>();
+
+                    // Search all skill that benefit by the removed recipe
+                    foreach (var benefit in skillBenefitsTooltips)
+                    {
+                        for (int i = 0; i < benefit.Value.Count; i++)
+                        {
+                            var b = benefit.Value.ElementAt(i);
+
+                            // if the benefit is for a recipe
+                            if (b.Key == targetRecipe.UILink())
+                            {
+                                //KeyA.Add(benefit.Key);
+                                //KeyB.Add(b.Key);
+                                b.Value.Clear();
+                                // Update benefit list
+                                benefit.Value[b.Key] = b.Value;
+                                continue;
+                            }
+
+                            //if not we try to found benefit for an item
+                            foreach (var item in targetRecipe.Products)
+                            {
+                                LocString itemLocString = item.Item.UILink();
+
+                                // if this benefit is benefit for an item on the recipe product
+                                if (b.Key == itemLocString)
+                                {
+                                    //KeyA.Add(benefit.Key);
+                                    //KeyB.Add(b.Key);
+
+                                    // clear all the benefit for this recipe
+                                    b.Value.Clear();
+                                    // Update benefit list
+                                    benefit.Value[b.Key] = b.Value;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    /*
+                    for (int i = 0; i < KeyA.Count; i++)
+                    {
+                        Console.WriteLine(KeyA[i]);
+                        Console.WriteLine(KeyA[i]);
+                        skillBenefitsTooltips[KeyA[i]][KeyB[i]].ForEach(x => Console.WriteLine(x));
+                    }
+                    */
                 }
+
             }
         }
+
     }
 }
